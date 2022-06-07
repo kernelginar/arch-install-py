3b250e29f075b5e29b7bde91913b8c4d3b117bbf
Abdullah <80760611+abdullah-rgb@users.noreply.github.com>
2022-06-07 23:41:33 +0300

    Update artix-install.py

--- artix-install.py
+++ artix-install.py
@@ -9,15 +9,15 @@ os.system("cfdisk " f"{disk_selection}")
 
 os.system("clear")
 os.system("lsblk")
-root_partition = input("/dev/???: ")
+root_partition = input("Root partition, /dev/???: ")
 os.system("mkfs.ext4 " f"{root_partition}")
 os.system("mount " f"{root_partition} " "/mnt")
 print(" ")
 
-boot_partition = input("/dev/???: ")
+boot_partition = input("Boot partition, /dev/???: ")
 os.system("mkfs.fat -F32 " f"{boot_partition}")
 os.system("mkdir -p /mnt/boot/efi")
 os.system("mount " f"{boot_partition} " "/mnt/boot/efi")
 
 os.system("clear")
-os.system("basestrap -i base base-devel linux linux-headers linux-firmware git nano artix-archlinux-support")
+os.system("basestrap -i /mnt base base-devel linux linux-headers linux-firmware git nano artix-archlinux-support")
