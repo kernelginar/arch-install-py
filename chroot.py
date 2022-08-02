658b4d03cb292c3a152178c7943955d86176d3e4
Abdullah <80760611+abdullah-rgb@users.noreply.github.com>
2022-08-02 03:03:21 +0300

    Update chroot.py

--- chroot.py
+++ chroot.py
@@ -6,7 +6,7 @@ os.system("clear")
 
 print("Example: Europe/Istanbul, Europe/Zurich, America/New_York")
 timezone = input("Timezone: ")
-os.system("ln -sf /usr/share/zoneinfo" f"{timezone}" "/etc/localtime")
+os.system("ln -sf /usr/share/zoneinfo/" f"{timezone} " "/etc/localtime")
 os.system("timedatectl set-ntp true")
 os.system("hwclock --systohc")
 
